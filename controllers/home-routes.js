d00757c40a1ee389b75e11e90b41cd8abf1279a1
Neon Riot <sbrookshire99@gmail.com>
2021-06-01 16:22:31 -0500

    Built server, created Post model, started home0-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { Post } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.render("all", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
