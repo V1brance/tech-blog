045810919cd1bf044ea231a0f4440c52d298779a
Neon Riot <sbrookshire99@gmail.com>
2021-06-01 19:37:34 -0500

    Basic routes now render text to page based on logged in status

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -23,6 +23,37 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/dashboard", async (req, res) => {
+  try {
+    let userPostData = {};
+    if (req.session.loggedIn) {
+      userPostData = await Post.findAll({
+        where: {
+          user_id: req.body.user_id,
+        },
+      });
+    } else {
+      userPostData = await Post.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["username"],
+          },
+        ],
+      });
+    }
+    const posts = userPostData.map((post) => post.get({ plain: true }));
+
+    res.render("dashboard", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
